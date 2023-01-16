f0c700aa9b3a414a88969d1316e200df1b16c2ed
Meret Fischli <merfisch@bluewin.ch>
2023-01-16 11:42:39 +0100

    newText

--- new.js
+++ new.js
@@ -1,6 +1,6 @@
 //fxhash seed
 let alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
-var fxhash =
+let fxhash =
   "oo" +
   Array(49)
     .fill(0)
@@ -24,7 +24,7 @@ let sfc32 = (a, b, c, d) => {
     b |= 0;
     c |= 0;
     d |= 0;
-    var t = (((a + b) | 0) + d) | 0;
+    let t = (((a + b) | 0) + d) | 0;
     d = (d + 1) | 0;
     a = b ^ (b >>> 9);
     b = (c + (c << 3)) | 0;
@@ -33,33 +33,27 @@ let sfc32 = (a, b, c, d) => {
     return (t >>> 0) / 4294967296;
   };
 };
-var fxrand = sfc32(...hashes);
+let fxrand = sfc32(...hashes);
 
-//zuerst immer den NameSpace mitgeben
+//––––––––––– zuerst immer den NameSpace mitgeben
 const ns = "http://www.w3.org/2000/svg";
 
 const svg = document.createElementNS(ns, "svg");
 
-//svg spezifisches Attribut viewBox um es responsive zu machen
+//––––––––––– svg Viewbox
 svg.setAttribute("viewBox", "0 0 1000 1000");
 document.body.append(svg);
 
-//const um random zu definieren und dann ersetzen mit random von fxhash
-// const r = Math.random;
+var draw = SVG(svg).addTo("#drawing");
+
 const r = fxrand;
 
-//zufällige Zahlen definieren
 const colors = [
   ["#39FF14", "#FF00FF", "#F1C40F", "#E67E22"],
-  ["#16A085", "#2980B9", "#8E44AD", "#2C3E50"],
-  ["#7F8C8D", "#F39C12", "#00FF00", "#FF00FF"],
-  ["#2980B9", "#5730F9", "#203", "#FF1C4C"],
+  ["#16A085", "#FF4501", "#8E44AD", "#2C3E50"],
+  ["#7F8C8D", "#F39C12", "#00FF00", "##220033"],
+  ["#FF4501", "#5730F9", "#220033", "#FF1C4C"],
 ];
-
-// const colors = [
-//     ["#FFFFFF", "#000000"],
-//     ["#000000", "#FFFFFF"],
-//   ];
 
 const pick = (d) => d[Math.floor(r() * d.length)];
 
@@ -86,20 +80,24 @@ const line = (position) => {
   //Zahlen im Array zu String verbinden mit Leerschlägen und Komma, damit wir sie nacher als Linie zusammensetzen können
   const tmp = points.map((d) => d.join(" ")).join(",");
 
+  const str = `M ${points[0][0]} ${points[0][1]} L ${tmp}`;
 
-  var input = document.querySelector("input[type=text]");
-  var draw = SVG().addTo("#drawing");
-  var text = draw.text(function (add) {
+  // p.setAttribute("d", str)
+  // p.setAttribute("fill", "none")
+  // p.setAttribute("stroke-width", "100")
+  // p.setAttribute("stroke", pick(pick(colors)))
+  // svg.append(p)
+
+  let input = document.querySelector("input[type=text]");
+  let text = draw.text(function (add) {
     add.tspan(input.value);
   });
 
-  textPath = text.path(
-    `M ${points[0][0]} ${points[0][1]} C ${tmp} S ${points[2][0]} ${points[2][1]}, 180 80`
-  );
+  let textPath = text.path(str);
 
-  textPath.animate(1000).ease('<>')
-  	.plot(`M ${points[0][1]} ${points[0][0]} C ${tmp} S ${points[2][1]} ${points[2][0]}, 80 180 `)
-  	.loop(true, true)
+  // textPath.animate(1000).ease('<>')
+  // 	.plot(`M ${tmp}`)
+  // 	.loop(true, true)
 
   input.addEventListener("keyup", updateText(textPath));
 
@@ -108,21 +106,43 @@ const line = (position) => {
       textPath.tspan(this.value);
     };
   }
+  text.font({ fill: "blue", family: "Helvetica", size: "100" });
+
+  svg.append(textPath);
 };
 
+//Circles
+
+function ellipse(x, y, z) {
+  const circle = document.createElementNS(ns, "circle");
+  //diese attribute müssen so heissen "cx", "cy", "r"
+  circle.setAttribute("cx", x);
+  circle.setAttribute("cy", y);
+  circle.setAttribute("r", z);
+  circle.setAttribute("fill", "red");
+
+  svg.append(circle);
+}
+
+for (let i = 0; i < 100; i++) {
+  z = Math.random() * 100;
+  x = Math.random() * (1000 - z);
+  y = Math.random() * (1000 - z);
+  ellipse(x, y, z);
+}
+
+
 //Anfangskoordinaten M
-const total = Math.floor(r() * 50 + 37);
-Array(total)
+// const total = Math.floor(r() * 50 + 37);
+Array(10)
   .fill(0)
   .map((d) => {
     //M bedeutet moveTo, L heisst lineTo
     const x = r() * 1000;
-    const y = r() * 30;
+    const y = r() * 500 - 300;
 
     line([x, y]);
   });
-
-  
 
 // –––––––––––– download and save svg
 
@@ -144,7 +164,18 @@ const save = (svg) => {
   download(new Blob([str], mime));
 };
 
-const click = (e) => {
-  save(svg);
+// mit Mouse click
+// const click = (e) => {
+//   save(svg);
+// };
+// document.addEventListener("click", click);
+
+// mit s key
+const keyHandler = (event) => {
+  console.log(event.key);
+  if (event.key === "s") {
+    save(svg);
+  }
 };
-document.addEventListener("click", click);
+
+document.addEventListener("keypress", keyHandler);
